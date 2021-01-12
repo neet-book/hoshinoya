6c934ab4a151173ac4a3e550fb9e27ff753d4bf1
erika <yanoerika@163.com>
2021-01-12 09:31:22 +0800

    创建项目

---
+++ pages/Swiper.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="swiper">
+
+    </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'nuxt-property-decorator'
+@Component
+export default class Swiper extends Vue
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="container">
+    
+  </div>
+</template>
+
+<script>
+import { Component, Vue } from 'nuxt-property-decorator'
+
+
+@Component({
+  components: {
+    
+  }
+})
+export default class Index extends Vue {}
+</script>
+
+<style>
+
+</style>
