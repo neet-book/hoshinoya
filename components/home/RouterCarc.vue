3ab242fe465507b1366c787cc3e078f39a47dd0b
yanoerika <yanoerika@163.com>
2021-02-20 17:03:23 +0800

    基本布局

---
+++ components/home/RouterCarc.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="router-card">
+      <div class="card-container">
+          <div class="card-name"></div>
+          <div class="card-logo"></div>
+          <p class="disc"></p>
+      </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'nuxt-property-decorator'
+
+@Component
+export default class RouterCard extends Vue {
+    @Prop(Object) content: any
+}
+</script>
+
+<style >
+
+</style>
