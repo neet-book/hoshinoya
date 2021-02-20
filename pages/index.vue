333fab85a84c15d2ea06a512efb3914577c0e1d4
yanoerika <yanoerika@163.com>
2021-02-20 16:55:48 +0800

    完成

--- pages/index.vue
+++ pages/index.vue
@@ -3,14 +3,20 @@
     <div class="top-head">
       <swiper :slider-list="hotels" class="top-slider" v-on:slider-change="currentSlider = $event" />
       <!-- logo -->
-      <div class="top-head-logo">
-        <div class="top-logoanime">
-          <div class="logo-border"></div>
-          <logo-anime :logos="hotels" :current="currentSlider" />
+      <div class="top-head-logo-container">
+        <div class="top-head-logos">
+          <div class="top-logoanime">
+            <div class="logo-border"></div>
+            <logo-anime :logos="hotels" :current="currentSlider" />
+          </div>
+          <!-- 文字logo -->
+          <svg class="top-head-logo-text"><use xlink:href="#logo-hoshinoya-text"></use></svg>
+          <!-- logo -->
+          <svg><use xlink:href="#logo-hoshinoya-mark"></use></svg>
         </div>
-        <svg><use xlink:href="#logo-hoshinoya-text"></use></svg>
-        <svg><use xlink:href="#logo-hoshinoya-mark"></use></svg>
+        <div class="top-head-pagetitle">虹夕诺雅</div>
       </div>
+      <div class="top-head-title">探索精髓之旅</div>
     </div>
     <div class="hotel-router">
 
@@ -84,32 +90,61 @@ export default class Index extends Vue {
   position: relative;
 }
 
-.top-head-logo {
-  position:absolute;
-  width: 52px;
+.top-head-logo-container {
+  position: absolute;
   top: 50%;
   left: 50%;
-
   transform: translate(-50%, -50%)
 }
 
+.top-head-logos, .top-head-pagetitle, .top-head-title {
+  position: relative;
+  left: 50%;
+  transform: translateX(-50%)
+}
+
+.top-head-logos {
+  width: 70px;
+}
+
+.top-head-logo-text {
+  margin: 10px 0;
+}
+
 .top-logoanime {
-  height: 52px;
-  height: 52px;
+  height: 70px;
+}
+
+.logo-border, .top-head-logos > svg {
+  width: 100%;
+  height: 70px;
 }
 
 .logo-border {
   box-sizing: border-box;
   position: absolute;
-  width: 52px;
-  height: 52px;
-  border: solid 6px white;
+  border: solid 8px white;
   border-radius: 50%;
 }
 
-.top-head-logo > svg{
-  width: 100%;
-  height: 60px;
+.top-head-logos > svg{
   fill: #fff;
+}
+
+.top-head-pagetitle {
+  margin-top: 20px;
+  color: #fff;
+  width: 20px;
+  font-size: 15px;
+  font-weight: 400;
+}
+
+.top-head-title {
+  letter-spacing: 5px;
+  left: 50%;
+  color: #fff;
+  position: absolute;
+  bottom: 170px;
+  transform: translateX(-50%)
 }
 </style>
