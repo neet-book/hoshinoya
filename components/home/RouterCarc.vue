e38bfd50cf76f18e8267b55d561430c99b71606c
yanoerika <yanoerika@163.com>
2021-02-25 17:36:36 +0800

    背景随鼠标移动

--- components/home/RouterCarc.vue
+++ components/home/RouterCarc.vue
@@ -2,11 +2,12 @@
   <div class="router-card">
     <nuxt-link calss='router-link' :to="'/hotel' + content.nameEN">
       <div class="router-card-container"
+        ref="container"
         @mousemove="onMousemove"
         @mouseleave="onMouseleave"
       >
         <div class="router-bg" 
-          :style="{ transform: 'translate(' + offsetX + 'px, ' + offsetY + 'px)' }"
+          :style="{ transform: 'translate(' + offsetX + '%, ' + offsetY + '%)' }"
         >
           <div 
           class="router-bg-img" 
@@ -37,30 +38,43 @@ export default class RouterCard extends Vue {
     positionX: number = 0
     positionY: number = 0
     mouseIn: boolean = false
+    displacementX: number = 0
+    displacementY: number = 0
 
     onMousemove(event: MouseEvent):void {
+      if (this.mouseIn === false) this.mouseIn = true
       const x = event.clientX
       const y = event.clientY
-      // 鼠标进入坐标
-      this.mouseIn = true
+      // 鼠标第一次进入
       if (this.positionX === 0 && this.positionY === 0) {
         this.positionY = y
         this.positionX = x
         return
       }
-      this.offsetX = (this.positionX - x)
-      this.offsetY = (this.positionY - y)
+      const container = this.$refs.container as HTMLElement
+      const containerWidth = container.clientWidth
+      const containerHeigh = container.clientHeight
 
-      
+      const displacementX = this.displacementX + (x - this.positionX)
+      const displacementY = this.displacementY + (y - this.positionY)
+
+      this.offsetX = Math.round((displacementX / containerWidth) * 100) / 100
+      this.offsetY = Math.round((displacementY / containerHeigh) * 100) / 100
+      this.positionX = x
+      this.positionY = y
+      this.displacementX = displacementX
+      this.displacementY = displacementY
     }
 
     onMouseleave():void {
       this.mouseIn = false
-      // 坐标重新初始化， 坐标复位
+      // 坐标重新初始化
       this.positionY = 0
       this.positionX = 0
       this.offsetX = 0
       this.offsetY = 0
+      this.displacementX = 0
+      this.displacementY = 0
     }
 }
 </script>
@@ -96,10 +110,12 @@ export default class RouterCard extends Vue {
 .router-info.active  {
   background-color: rgb(0 0 0 / 57%);
 }
+
 .router-bg {
   position: relative;
   top: 0;
   left: 0;
+  transform: translate(0px, 0px);
 }
 
 .router-bg-img {
