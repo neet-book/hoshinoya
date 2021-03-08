9f79812405b95af77aa851a7837f41363de1f525
yanoerika <yanoerika@Sou.localdomain>
2021-03-08 18:06:57 +0800

    创建预订组件

--- pages/hotel/_hotel_name/index.vue
+++ pages/hotel/_hotel_name/index.vue
@@ -1,14 +1,22 @@
 <template>
   <div class="hotel">
-    {{ $route.params.hotel_name }}
+    <header>
+        <div class="hotel-header-container">
+            <div class="hotel-header-logo"></div>
+            <div class="hotel-header-title"></div>
+            <div class="hotel-header-disc"></div>
+        </div>
+    </header>
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue, Prop } from 'nuxt-property-decorator'
+import { Component, Vue } from 'nuxt-property-decorator'
 
-@Component
-export default class LogoAnime extends Vue {
+@Component({
+    layout: 'hotel'
+})
+export default class Hotel extends Vue {
   
 }
 </script>
