02b40a5eb4c8955070a8db367765c299f2785717
yanoerika <yanoerika@Sou.localdomain>
2021-03-10 11:33:34 +0800

    menue_bar组件完成

--- components/hotel/HotelBookingMenu.vue
+++ components/hotel/HotelBookingMenu.vue
@@ -1,11 +1,16 @@
 <template>
   <div class="booking-menu">
-    <div class="menu-bar">
+    <div class="menu-bar" @click="onClick">
       <div class="menu-bar-icon">
-        <div class="menu-bar-icon-menu"></div>
-        <div class="menu-bar-icon-close"></div>
+        <div class="menu-bar-icon-menu" :class="{ active: !active }">
+          <div class="icon-line"></div>
+          <div class="icon-line"></div>
+          <div class="icon-line"></div>
+        </div>
+        <div class="menu-bar-icon-close" :class="{ active: active }">
+        </div>
       </div>
-      <div class="nemu-bar-name">HOSHINOYA {{ hotel | nameFilter }}</div>
+      <div class="nemu-bar-name" :class="{ show: showName }">HOSHINOYA {{ hotel | nameFilter }}</div>
     </div>
   </div>
 </template>
@@ -20,6 +25,13 @@ import { Component, Vue, Prop } from 'nuxt-property-decorator'
 })
 export default class HotelBookingMenu extends Vue {
   @Prop(String) hotel: string | undefined
+  @Prop(Boolean) showName: boolean | undefined
+  active: boolean = false
+
+  onClick(): void {
+    this.active = !this.active
+    this.$emit('menuo-open', this.active)
+  }
 }
 </script>
 
@@ -29,6 +41,8 @@ export default class HotelBookingMenu extends Vue {
   position: absolute;
   height: 100%;
   width: 110px;
+  cursor: pointer;
+  transition: background-color 200ms cubic-bezier(0.165, 0.84, 0.44, 1);
 }
 
 .menu-bar:hover {
@@ -58,17 +72,58 @@ export default class HotelBookingMenu extends Vue {
   letter-spacing: 1.8px;
   word-break: break-all;
   text-align: center;
+  opacity: 0;
+  transition: opacity 900ms cubic-bezier(0.445,0.05,0.55,0.95) 200ms;
+}
+
+.nemu-bar-name.show {
+  opacity: 1;
 }
 
 .menu-bar-icon {
   position: absolute;
   width: 38px;
   height: 38px;
+  top: 36px;
   left: 50%;
-  transform: translateY(-50%);
+  transform: translateX(-50%);
 }
 
-.menu-bar-icon-nemu {
-  background-image: url('');
+.menu-bar-icon-menu {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  transform: scale(0, 1);
+  transition: width 400ms cubic-bezier(.165,.84,.44,1) 100ms;
 }
+
+.menu-bar-icon-menu.active {
+  transform: scale(1, 1);
+  transition: transform 100ms cubic-bezier(.165,.84,.44,1);
+}
+
+.icon-line {
+  width: 100%;
+  height: 1px;
+  background: black;
+}
+
+.menu-bar-icon-close {
+  width: 100%;
+  height: 100%;
+  background: url('~assets/svg/close.svg');
+  transform: scale(0, 0); 
+  transition: transform 100ms cubic-bezier(.165,.84,.44,1);
+}
+
+.menu-bar-icon-close.active {
+  transform: scale(1, 1);
+  transition: transform 400ms cubic-bezier(.165,.84,.44,1) 100ms
+}
+
+
 </style>
