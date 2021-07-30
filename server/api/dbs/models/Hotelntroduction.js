18ccebb143b3c10d5dc3de15ed207459f668dc61
yanoerika <yanoerika@Sou.localdomain>
2021-07-30 11:25:47 +0800

    定义hote_introductions集合，创建introduction接口

---
+++ server/api/dbs/models/Hotelntroduction.js
@@ -0,0 +1,84 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const HotelIntroductionSchema = new Schema({
+  hotelID: Number,
+  hotelName: String,
+  hotelNameEn: String,
+  topSection: {
+    title: String,
+    content: String,
+    backgroundImage: {
+      normal: String,
+      big: String,
+      square: String
+    }
+  },
+  largeSection: {
+    title: String,
+    content: String,
+    circleImage: {
+      normal: String,
+      big: String,
+      square: String
+    },
+    mapImage : {
+      normal: String,
+      big: String,
+      square: String
+    }
+  },
+  outlineFirstSection: {
+    title: String,
+    carrouselImtes: [
+      {
+        title: String,
+        image: {
+          normal: String,
+          big: String
+        }  
+      }
+    ],
+    disc: String
+  },
+  outlineSecondSection: {
+    title: String,
+    grids: [
+      {
+        image: {
+          normal: String,
+          big: String
+        }
+      }
+    ],
+    disc: String,
+    subDisc: {
+      title: String,
+      content: String
+    }
+  },
+  outlineThirdSection: {
+    title: String,
+    carrouselImtes: [
+      {
+        title: String,
+        image: {
+          normal: String,
+          big: String
+        }  
+      }
+    ],
+    disc: String,
+    subDisc: {
+      title: String,
+      content: String
+    }
+  }
+
+})
+
+const collection = 'hotel_introductions'
+
+HotelIntroductionSchema.set('collection', collection)
+
+module.exports = mongoose.model(collection, HotelIntroductionSchema)
